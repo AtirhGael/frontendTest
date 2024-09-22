99fc155a19f33b852d2748b97f94d858a25e2500
AtirhGael <atirhg@gmail.com>
2024-09-22 13:28:12 +0100

    protected

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-// import './App.css'
-import { Provider } from 'react-redux'
-import store from './redux/store'
-import {
-  BrowserRouter
-} from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import Authentication from './pages/Auth';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
-
   return (
-    <>
-      <Provider store={store}>
+    <Provider store={store}>
       <BrowserRouter>
-        <MainPage />
-        <ToastContainer/>
+        <Routes>
+          {/* Unprotected route for login/signup */}
+          <Route path="/" element={<Authentication />} />
+          
+          {/* Protected routes: only accessible when authenticated */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/*" element={<MainPage />} /> {/* Catch-all for MainPage routes */}
+          </Route>
+        </Routes>
+        <ToastContainer />
       </BrowserRouter>
-      </Provider>
-    </>
-  )
+    </Provider>
+  );
 }
 
-export default App
+export default App;
--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
-import { RootState } from '../redux/store';
+import { RootState } from './redux/store';
 
 const ProtectedRoute: React.FC = () => {
-  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+  const isAuthenticated = useSelector((state: RootState) => state.authSlice.isAuthenticated);
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
 };
